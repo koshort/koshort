e5c5eede8a804303edbbbd1e19223d14020e78d7
Jung <iam@nyanye.com>
2018-04-24 18:44:41 +0900

    initial init

---
+++ koshort/__init__.py
@@ -0,0 +1,12 @@
+import pkg_resources
+
+
+__title__ = 'koshort'
+__author__ = 'nyanye'
+__license__ = 'GPL v3'
+__copyright__ = 'Copyright 2018 Nyanye'
+
+try:
+    __version__ = pkg_resources.get_distribution('koshort')
+except pkg_resources.DistributionNotFound:
+    __version__ = "dev"
