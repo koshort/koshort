9869750319577534109c963b6353e6598b09ed18
nyanye <iam@nyanye.com>
2018-04-25 23:23:33 +0900

    glob > os.listdir

--- tests/test_koshort_ops.py
+++ tests/test_koshort_ops.py
@@ -1,10 +1,10 @@
 import koshort
-import glob
+import os
 from koshort.constants import DATA_DIR
 
 def test_clear_data():
     """Check if file is correctly cleared."""
 
     koshort.clear()
-    items = glob.glob(DATA_DIR+"*")
+    items = os.listdir(DATA_DIR)
     assert len(items) == 0
