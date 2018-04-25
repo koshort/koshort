7d6b1ff7ddeb94b68eb3d823d4d92aeb3a624df0
nyanye <iam@nyanye.com>
2018-04-25 23:23:16 +0900

    updated example

--- stream_twitter.py
+++ stream_twitter.py
@@ -1,5 +1,10 @@
 from koshort.stream import TwitterStreamer
 
 if __name__ == '__main__':
-    app = TwitterStreamer("data/", ["그냥", "오늘", "치킨이", "먹고싶다"])
+    app = TwitterStreamer("data/", ["가","나","다","라"])
+    # app.args.remove_links = True
+    # app.args.remove_mentions = True
+    # app.args.verbose = True
+    # app.args.tweet_limits = 1000
+    app.create_listener()
     app.run()
