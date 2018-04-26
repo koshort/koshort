3611f61fc6475ca9cdba662e9cb0f9a546b553be
Jung <iam@nyanye.com>
2018-04-26 18:26:44 +0900

    Add test_koshort_naver

---
+++ tests/test_koshort_naver.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
+from koshort.stream.naver import NaverStreamer
+from koshort.constants import DATA_DIR
+import glob
+
+
+def test_naver_streamer():
+    naver = NaverStreamer()
+    naver.args.n_limits = 1
+    naver.args.display_rank = True
+    naver.args.verbose = True
+    naver.stream(async=False, interval=0)
+
+
+def test_result_exists():
+    """Check if files are correctly created. """
+
+    items = glob.glob(DATA_DIR+"*trend*")
+    assert len(items) > 0
