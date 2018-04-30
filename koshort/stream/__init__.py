b67c51a593ce4a2c78acde86ce237ec1ca1ce1d3
nyanye <iam@nyanye.com>
2018-05-01 00:26:47 +0900

    Change InteractiveStreamer to NavtterStreamer

--- koshort/stream/__init__.py
+++ koshort/stream/__init__.py
@@ -3,4 +3,4 @@ from __future__ import absolute_import
 from koshort.stream.base import BaseStreamer
 from koshort.stream.twitter import TwitterStreamer
 from koshort.stream.naver import NaverStreamer
-from koshort.stream.misc import InteractiveStreamer
+from koshort.stream.misc import NavtterStreamer
