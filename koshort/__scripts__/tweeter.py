204decc353eccbd9636075aa4fcd3b1df92929e0
nyanye <iam@nyanye.com>
2018-05-12 22:02:11 +0900

    Update tweeter script

--- koshort/__scripts__/tweeter.py
+++ koshort/__scripts__/tweeter.py
@@ -1,11 +1,12 @@
 from koshort.stream import TwitterStreamer
 
+
 def main():
-    app = TwitterStreamer(async=False)
+    app = TwitterStreamer(is_async=False)
     app.options.verbose = True
     app.show_options()
     app.create_listener()
-    app.stream(async=False)
+    app.stream()
 
 
 if __name__ == '__main__':
