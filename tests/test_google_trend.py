5ae58a52ad31c12b256bae50e9be39726c42197b
nyanye <iam@nyanye.com>
2018-06-09 17:28:44 +0900

    Add FIXME

--- tests/test_google_trend.py
+++ tests/test_google_trend.py
@@ -6,7 +6,7 @@ from __future__ import division
 from koshort.stream import GoogleTrendStreamer
 import pytest
 
-
+# FIXME: init_date가 고정되어 시간이 지났을 때 test에 실패할 가능성이 있음.
 @pytest.mark.parametrize("is_async", [(True), (False)])
 def test_google_trend_streamer(is_async):
     streamer = GoogleTrendStreamer(is_async=is_async)
